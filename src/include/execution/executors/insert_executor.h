54972da5e4a974af5da24b2259e864aa3fbe31bf
Trung Pham <htsilvakt04@gmail.com>
2023-08-15 07:10:59 +0700

    done insert.

--- src/include/execution/executors/insert_executor.h
+++ src/include/execution/executors/insert_executor.h
@@ -57,6 +57,9 @@ class InsertExecutor : public AbstractExecutor {
  private:
   /** The insert plan node to be executed*/
   const InsertPlanNode *plan_;
+  /** The child executor from which tuples are obtained */
+  std::unique_ptr<AbstractExecutor> child_executor_;
+  bool done_{false};
 };
 
 }  // namespace bustub
